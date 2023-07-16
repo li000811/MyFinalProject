5b437442d91cc45172908f917621a50b67041033
Changhong Li <li000811@algonquinlive.com>
2023-07-16 18:58:50 -0400

    Initial commit

---
+++ app/src/main/java/algonquin/cst2335/myfinalproject/MainActivity.java
@@ -0,0 +1,33 @@
+package algonquin.cst2335.myfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import algonquin.cst2335.myfinalproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.idAviationTracker.setOnClickListener( click ->
+                startActivity(new Intent(this, IdAviation.class))
+        );
+        binding.idCurrencyConverter.setOnClickListener( click ->
+                startActivity(new Intent(this, IdCurrencyConverter.class))
+        );
+        binding.idTriviaQuestion.setOnClickListener( click ->
+                startActivity(new Intent(this, IdTriviaQuestion.class))
+        );
+        binding.idBearGenerator.setOnClickListener( click ->
+                startActivity(new Intent(this, IdBearGenerator.class))
+        );
+
+    }
+}
