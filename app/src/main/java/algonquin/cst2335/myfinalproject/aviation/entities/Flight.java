59b388cddee7322dd7b2aa8e7472e59216ea8df2
Ahmad Al-Jabbouri <alja0062@algonquinlive.com>
2023-08-03 12:38:09 -0400

    SearchFragment complete

--- app/src/main/java/algonquin/cst2335/myfinalproject/aviation/entities/Flight.java
+++ app/src/main/java/algonquin/cst2335/myfinalproject/aviation/entities/Flight.java
@@ -1,139 +1,23 @@
 package algonquin.cst2335.myfinalproject.aviation.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-/**
- * Represents a Flight object containing information about a departure from the airport that was
- * searched. A Flight has an origin, a destination, a terminal, a gate, and a delay (if any).
- */
+import algonquin.cst2335.myfinalproject.aviation.DTO.DataDTO;
+import algonquin.cst2335.myfinalproject.aviation.DTO.PaginationDTO;
+
 public class Flight {
 
-    // Cards
-    private LocalDateTime datetime;
-    private String origin;
-    private String destination;
-    private String airport;
-    // Details
-    private String terminal;
-    private String gate;
-    private int delay;
-    private String flightNo;
+    private PaginationDTO pagination;
+    private List<DataDTO> data;
 
-    /**
-     * Default constructor for the Flight class.
-     */
-    public Flight() {}
-
-    /**
-     * Constructs a Flight object with specified origin, destination, terminal, and gate.
-     *
-     * @param origin      The airport code representing the flight's origin.
-     * @param destination The airport code representing the flight's destination.
-     * @param terminal    The terminal at the origin airport for the flight.
-     * @param gate        The gate at the origin airport for the flight.
-     */
-    public Flight(String origin, String destination, String terminal, String gate) {
-        this.origin = origin;
-        this.destination = destination;
-        this.terminal = terminal;
-        this.gate = gate;
+    public PaginationDTO getPagination() {
+        return pagination;
     }
-
-    /**
-     * Get the airport code representing the flight's origin.
-     *
-     * @return The origin airport code as a String.
-     */
-    public String getOrigin() {
-        return origin;
+    public void setPagination(PaginationDTO pagination) {
+        this.pagination = pagination;
     }
-
-    /**
-     * Set the airport code representing the flight's origin.
-     *
-     * @param origin The origin airport code to be set.
-     * @return The Flight object to enable method chaining.
-     */
-    public Flight setOrigin(String origin) {
-        this.origin = origin;
-        return this;
-    }
-
-    /**
-     * Get the airport code representing the flight's destination.
-     *
-     * @return The destination airport code as a String.
-     */
-    public String getDestination() {
-        return destination;
-    }
-
-    /**
-     * Set the airport code representing the flight's destination.
-     *
-     * @param destination The destination airport code to be set.
-     * @return The Flight object to enable method chaining.
-     */
-    public Flight setDestination(String destination) {
-        this.destination = destination;
-        return this;
-    }
-
-    /**
-     * Get the terminal at the origin airport for the flight.
-     *
-     * @return The terminal at the origin airport as a String.
-     */
-    public String getTerminal() {
-        return terminal;
-    }
-
-    /**
-     * Set the terminal at the origin airport for the flight.
-     *
-     * @param terminal The terminal at the origin airport to be set.
-     * @return The Flight object to enable method chaining.
-     */
-    public Flight setTerminal(String terminal) {
-        this.terminal = terminal;
-        return this;
-    }
-
-    /**
-     * Get the gate at the origin airport for the flight.
-     *
-     * @return The gate at the origin airport as a String.
-     */
-    public String getGate() {
-        return gate;
-    }
-
-    /**
-     * Set the gate at the origin airport for the flight.
-     *
-     * @param gate The gate at the origin airport to be set.
-     * @return The Flight object to enable method chaining.
-     */
-    public Flight setGate(String gate) {
-        this.gate = gate;
-        return this;
-    }
-
-    /**
-     * Get the delay status for the flight (if any).
-     *
-     * @return The delay status as a String, or null if no delay.
-     */
-    public int getDelay() {return delay;}
-
-    /**
-     * Set the delay status for the flight.
-     *
-     * @param delay The delay status to be set.
-     * @return The Flight object to enable method chaining.
-     */
-    public Flight setDelay(int delay) {
-        this.delay = delay;
-        return this;
+    public List<DataDTO> getData() {
+        return data;
     }
 }
--- app/src/main/java/algonquin/cst2335/myfinalproject/aviation/fragments/SearchFragment.java
+++ app/src/main/java/algonquin/cst2335/myfinalproject/aviation/fragments/SearchFragment.java
@@ -21,14 +21,22 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import algonquin.cst2335.myfinalproject.R;
+import algonquin.cst2335.myfinalproject.aviation.DTO.DataDTO;
+import algonquin.cst2335.myfinalproject.aviation.IdAviation;
 import algonquin.cst2335.myfinalproject.aviation.adapters.FlightListAdapter;
+import algonquin.cst2335.myfinalproject.aviation.entities.Flight;
 import algonquin.cst2335.myfinalproject.aviation.interfaces.OnItemClickListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
 
 public class SearchFragment extends Fragment {
 
@@ -69,15 +77,12 @@ public class SearchFragment extends Fragment {
             requestData(IATAcode);
         });
 
-        mAdapter.setListener(new OnItemClickListener() {
-            @Override
-            public void callBack(FightsBean.DataDTO dataDTO) {
+        mAdapter.setListener(dataDTO -> {
 
-                Intent intent = new Intent(getContext(), FightActivity.class);
-                String data = mGson.toJson(dataDTO);
-                intent.putExtra("data", data);
-                startActivity(intent);
-            }
+            Intent intent = new Intent(getContext(), IdAviation.class);
+            String data = mGson.toJson(dataDTO);
+            intent.putExtra("data", data);
+            startActivity(intent);
         });
 
         String key = sharedPreferences.getString("KEY", "");
@@ -88,14 +93,14 @@ public class SearchFragment extends Fragment {
         AlertDialog loadingDialog = new AlertDialog.Builder(getContext()).setMessage(getContext().getString(R.string.loading)).create();
         loadingDialog.show();
         RequestQueue mQueue = Volley.newRequestQueue(getContext());
-        StringRequest stringRequest = new StringRequest("http://api.aviationstack.com/v1/flights?access_key=" + access_key + "&dep_iata=" + key, response -> {
+        StringRequest stringRequest = new StringRequest("http://api.aviationstack.com/v1/flights?access_key=" + KEY + "&dep_iata=" + key, response -> {
             loadingDialog.dismiss();
             Snackbar.make(mClRoot, getContext().getString(R.string.success), Snackbar.LENGTH_SHORT).show();
             Gson gson = new Gson();
             try {
-                FightsBean data = gson.fromJson(response, FightsBean.class);
-                List<FightsBean.DataDTO> d = data.getData();
-                List<FightsBean.DataDTO> reallyData = new ArrayList<>();
+                Flight data = gson.fromJson(response, Flight.class);
+                List<DataDTO> d = data.getData();
+                List<DataDTO> reallyData = new ArrayList<>();
                 for (int i = 0; i < d.size(); i++) {
                     if (d.get(i).getFlight() != null) {
                         reallyData.add(d.get(i));
